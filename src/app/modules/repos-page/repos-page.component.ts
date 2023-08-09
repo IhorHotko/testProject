a94edc2ba6638adb365892821c59b39ce95544c7
IhorHotko <ihor.hotko@israilit.pro>
2023-08-09 23:31:54 +0300

    Create repos modules and components

---
+++ src/app/modules/repos-page/repos-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repos-page',
+  templateUrl: './repos-page.component.html',
+  styleUrls: ['./repos-page.component.scss']
+})
+export class ReposPageComponent {
+
+}
---
+++ src/app/modules/repos-page/repos-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReposPageComponent } from './repos-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ReposPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReposPageModule { }
